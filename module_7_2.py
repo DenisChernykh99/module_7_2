15c2daee7e3106072451a70d3ac7ca7e5975ccbe
DenisChernykh99 <hegetoer99@gmail.com>
2024-12-11 12:08:02 +0300

    Add files via upload

---
+++ module_7_2.py
@@ -0,0 +1,27 @@
+import os
+
+
+def custom_write(file_name, strings):
+    strings_positions = {}
+    number_string = 1
+    if not os.path.exists(file_name):
+        with open(file_name, 'w') as file:
+            pass  # создаем пустой файл, если его нет
+    file = open(file_name, 'a', encoding='utf-8')
+    for new_string in strings:
+        strings_positions[(number_string, file.tell())] = new_string
+        file.write(f'{new_string}\n')
+        number_string += 1
+    return strings_positions
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
